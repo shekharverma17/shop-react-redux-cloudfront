0abefc33a8f88672a05e6712fefda80d6bba6479
Shekhar Verma <Shekhar.Verma@cantire.com>
2022-06-30 15:06:52 +0530

    Serverless-finch and serverless-single-page-app plugins are added and configured and deployment is automated

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,8 @@ export default function Header() {
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">My Store!</Link>
+        <Link className={classes.homeLink} to="/">Serverless Cloud Basket!</Link><span style={{ fontSize: 'small', fontWeight: 300, display: 'block', color: 'yellow'}}>One place for all needs.</span>
+        
         </Typography>
 
         {auth && (
