724a9c300ce394ebbefeccdba67f2e2300e4ec7e
Shekhar Verma <shekhar17verma@gmail.com>
2022-08-29 21:09:51 +0530

    task-7 changes

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">Amazing Store AWS!</Link>
+          <Link className={classes.homeLink} to="/">Amazing Store!</Link>
         </Typography>
 
         {auth && (
